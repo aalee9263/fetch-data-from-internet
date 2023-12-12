509210345620e77bdf3d506f845bbc7494f149fa
aalee9263 <144584171+aalee9263@users.noreply.github.com>
2023-12-12 23:06:50 +0500

    error

--- object.js
+++ object.js
@@ -1,18 +1,18 @@
-// let std = [0, 23, 10, 31, 10,]///// Array
+// // let std = [0, 23, 10, 31, 10,]///// Array
 
-let std = {
+// let std = {
 
-    firstName: "Arshad",
-    lastName: "Ali",
-    age: 30,
-    class: "AICB",
-    Education: "B.Sc",
-    isMarried: true
-}
+//     firstName: "Arshad",
+//     lastName: "Ali",
+//     age: 30,
+//     class: "AICB",
+//     Education: "B.Sc",
+//     isMarried: true
+// }
 
-console.log(std["lastName"])
-console.log(std.firstName)
+// console.log(std["lastName"])
+// console.log(std.firstName)
 
-///Now we changes in objects..
+// ///Now we changes in objects..
 
-std.lastName = "aalee"
+// std.lastName = "aalee"
--- weather.js
+++
@@ -1,4 +0,0 @@
-let cityName = 
-url = https://api.openweathermap.org/data/2.5/weather?units=metric&appid=da8c26ed217416e63ccc33bfeb76ea80
-
-API key = 
