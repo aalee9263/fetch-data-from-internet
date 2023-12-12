eba3d526ceeb9c8f4fdc616c90f1a363e0eec030
aalee9263 <144584171+aalee9263@users.noreply.github.com>
2023-12-12 22:21:54 +0500

    added

---
+++ object.js
@@ -0,0 +1,18 @@
+// let std = [0, 23, 10, 31, 10,]///// Array
+
+let std = {
+
+    firstName: "Arshad",
+    lastName: "Ali",
+    age: 30,
+    class: "AICB",
+    Education: "B.Sc",
+    isMarried: true
+}
+
+console.log(std["lastName"])
+console.log(std.firstName)
+
+///Now we changes in objects..
+
+std.lastName = "aalee"
---
+++ weather.js
@@ -0,0 +1,4 @@
+let cityName = 
+url = https://api.openweathermap.org/data/2.5/weather?units=metric&appid=da8c26ed217416e63ccc33bfeb76ea80
+
+API key = 
